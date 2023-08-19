e567dd628b4b01f5f58565906905b34c73b10317
fernando predes <fernando_predes@hotmail.com>
2023-08-19 14:42:41 -0300

    criada a navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+interface StyledNavProps {
+  isScrolled: boolean;
+}
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <StyledNav isScrolled={isScrolled}>
+      <NavContainer>
+        <Logo src="/logo.png" alt="Logo" />
+        <NavLinks>
+          <NavLink>Home</NavLink>
+          <NavLink>Quem Somos</NavLink>
+          <NavLink>Produtos</NavLink>
+          <NavLink>Preços</NavLink>
+          <NavLink>Contato</NavLink>
+        </NavLinks>
+      </NavContainer>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav<StyledNavProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: ${({ isScrolled }) => (isScrolled ? 'transparent' : '#36558f')};
+  transition: background-color 0.3s ease-in-out;
+  z-index: 1000;
+`;
+
+const NavContainer = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const Logo = styled.img`
+  height: 40px;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLink = styled.a`
+  color: white;
+  text-decoration: none;
+  margin-left: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  &:hover {
+    color: #b0bed9;
+  }
+`;
+
+export default Navbar;
